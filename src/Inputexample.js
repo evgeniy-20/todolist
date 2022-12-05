f3dbb18331570aa2e7397cc4db9b879612081aa2
evgeniy20 <evgeniy.prokopiv@gmail.com>
2022-12-05 05:04:13 +0200

    handleClick

--- src/Inputexample.js
+++ src/Inputexample.js
@@ -2,14 +2,19 @@ import React, {useState} from 'react'
 
 function Inputexample() {
   const [value, setValue] = useState('')
+  const [result, setResult] = useState('')
 
   const handleChange  = (event) => {
     setValue(event.target.value);
   }
+  const handleClick = () => {
+    setResult(value);
+  }
   return (
     <div>
         <input value={value} onChange={handleChange} />
-		<p>Результат: {value}</p>
+		    <p>Результат: {result}</p>
+        <button onClick={handleClick}>Натисни на мене</button>
     </div>
   )
 }
